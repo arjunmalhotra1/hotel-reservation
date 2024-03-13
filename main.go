25c851678a22b2d8b54f1e22b8222ad870a05d70
arjunmalhotra1 <am2cj@virginia.edu>
2024-03-13 10:05:09 -0500

    add api grouping and listenAddr flag parsing

--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	listenAddr := flag.String("listenAddr", ":3000", "The listen address of the API server")
+	flag.Parse()
 
 	app := fiber.New()
 
 	app.Get("/foo", handleFoo)
+	apiv1 := app.Group("/api/v1")
 
-	err := app.Listen(":3000")
+	// We gorup the user endpoint with apiv1.
+	apiv1.Get("/user", handleUser)
+
+	err := app.Listen(*listenAddr)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -21,4 +28,8 @@ func main() {
 
 func handleFoo(c *fiber.Ctx) error {
 	return c.JSON(map[string]string{"msg": "working just fine"})
+}
+
+func handleUser(c *fiber.Ctx) error {
+	return c.JSON(map[string]string{"user": "James Foo"})
 }
