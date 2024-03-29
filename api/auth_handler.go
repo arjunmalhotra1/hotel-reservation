13049ce155acf35e5257ed65faa4472dea6577a2
arjunmalhotra1 <am2cj@virginia.edu>
2024-03-29 07:03:27 -0500

    Add functionality to authenticate a user

--- api/auth_handler.go
+++ api/auth_handler.go
@@ -1,10 +1,13 @@
 package api
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/arjunmalhotra1/hotel-reservation/db"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthHandler struct {
@@ -23,12 +26,25 @@ type AuthParams struct {
 }
 
 func (h *AuthHandler) HandleAuthenticate(c *fiber.Ctx) error {
-	var AuthParams AuthParams
-	if err := c.BodyParser(&AuthParams); err != nil {
+	var params AuthParams
+	if err := c.BodyParser(&params); err != nil {
 		return err
 	}
 
-	fmt.Println("AuthParams", AuthParams)
+	user, err := h.userStore.GetUserByEmail(c.Context(), params.Email)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return fmt.Errorf("invalid credentials")
+		}
+		return err
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.EncryptedPassword), []byte(params.Password))
+	if err != nil {
+		return fmt.Errorf("invalid credentials")
+	}
+
+	fmt.Println("authenticated ->", user)
 
 	return nil
 }
--- scripts/seed.go
+++ scripts/seed.go
@@ -15,8 +15,27 @@ var (
 	client     *mongo.Client
 	roomStore  db.RoomStore
 	hotelStore db.HotelStore
+	userStore  db.UserStore
 	ctx        = context.Background()
 )
+
+func seedUser(fname, lname, email string) {
+	user, err := types.NewUserFromParams(types.CreateUserParams{
+		Email:     email,
+		FirstName: fname,
+		LastName:  lname,
+		Password:  "supersecurepassword",
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = userStore.CreateUser(context.TODO(), user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
 
 func seedHotel(name, location string, rating int) {
 	hotel := types.Hotel{
@@ -60,6 +79,7 @@ func main() {
 	seedHotel("Bellucia", "France", 3)
 	seedHotel("The cozy hotel", "Netherlands", 4)
 	seedHotel("The cold hotel", "London", 1)
+	seedUser("james", "foo", "james@foo.com")
 
 }
 
@@ -76,4 +96,5 @@ func init() {
 
 	hotelStore = db.NewMongoHotelStore(client)
 	roomStore = db.NewMongoRoomStore(client, hotelStore)
+	userStore = db.NewMongoUserStore(client)
 }
